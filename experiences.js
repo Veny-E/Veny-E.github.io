7d473d0a1f63c7aa13a457c72b718b5e5b9910f0
Veny-E <thomas.vesseron.decouvreur@gmail.com>
2026-03-09 18:39:50 +0100

    corrections fautes ortho

--- experiences.js
+++ experiences.js
@@ -3,19 +3,19 @@ function stage(Id){
 
     if(Id=="Mieres"){
         image="pictures/mieres_logo.png";
-        text="En 2024, j'ai effectué lors des vacances de la Toussaint un stage d'observation dans l'entreprise Mieres IT à tregunc. Là, j'ai eu l'occasion d'obsverer les différentes missons d'une équipe de dévelopement informatique de découvrir les différents outils employés et d'essayer par moi-même certains outils. Ce stage ma conforté dans l'idée de travailler dans le domaine informatique. ";
+        text="En 2024, j'ai effectué lors des vacances de la Toussaint un stage d'observation dans l'entreprise Mieres IT à Tregunc. Là, j'ai eu l'occasion d'observer les différentes missions d'une équipe de développement informatique de découvrir les différents outils employés et d'essayer par moi-même certains outils. Ce stage m'a conforté dans l'idée de travailler dans le domaine informatique. ";
     }
     else if (Id=="Chat"){
         image="pictures/chat_miro.jpeg";
-        text="En 2022, J'ai effectué un stage dans la librairie le chat miro. A cette occasion j'ai pû consolider mon expériences professionnelleset mes compétences en matière d'organisation, de gestion d'inventaire et d'utilisation des différents logiciels qui servent à l'encaissement. ";
+        text="En 2022, j'ai effectué un stage dans la librairie Le Chat Miro. A cette occasion j'ai pu consolider mon expérience professionnelle et mes compétences en matière d'organisation, de gestion d'inventaire et d'utilisation des différents logiciels qui servent à l'encaissement. ";
     }
     else if(Id=="Geek"){
         image="pictures/geek_avenue.jpeg";
-        text="En 2023, J'ai effectué un stage dans la boutique Geek Avenue. Ainsi j'ai découvert un autre aspect du métier de la ventre. Notamment la gestion de livraison et d'envoi de colis.   ";
+        text="En 2023, j'ai effectué un stage dans la boutique Geek Avenue. Ainsi j'ai découvert un autre aspect du métier de la vente. Notamment la gestion de livraison et d'envoi de colis.   ";
     }
     else if (Id=="Méninge"){
         image="pictures/remue_meninge.png";
-        text="En 2022, j'ai effectué un stage dans la boutique le Remue Meninge. Là j'ai découvert la gestion d'une boutique indépendante, la gestion d'inventaire, la relation et la fidélisation clients. J'ai aussi pû aider à la mise en place du service ludothèque proposé par la boutique. ";
+        text="En 2022, j'ai effectué un stage dans la boutique le Remue Meninge. Là j'ai découvert la gestion d'une boutique indépendante, la gestion d'inventaire, la relation et la fidélisation clients. J'ai aussi pu aider à la mise en place du service ludothèque proposé par la boutique. ";
     }
 
     document.getElementById('monImage').src = image;
--- parcours.js
+++ parcours.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded",() => {
 
 const descriptions={
-    "picture-formation/diwan_tregon.1":"J'effectue ma primaire dans les écoles bretonnantes du réseau Diwan",
+    "picture-formation/diwan_tregon.1":"J'ai effectué ma primaire dans les écoles bretonnantes du réseau Diwan",
     "picture-formation/collège_stma.png":"Obtention du Brevet des collèges avec mention très bien",
     "picture-formation/lycée_stjo.avif":"Première : Maths/Physique/LLCE | Terminale : Maths/Physique | Obtention du baccalauréat mention très bien avec félicitations du jury ",
     "picture-formation/ensibs_lorient.jpg":"Préparation intégrée à l'ensibs, sous le cursus SNIO, où nous acquérons diverses compétences générales telles que des notions en C/Python/HTML/CSS/JS ou encore en mécanique, électronique...   ",
